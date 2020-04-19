487659e4f7c3cf7e6ad7580cb7ebf20f00643eab
Dominik Tyl <dominiktyl@Dominik-MacBook-Pro.local>
2020-04-19 09:35:35 +0200

    Auntentification level 4 brypt

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption")
+const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -19,9 +22,10 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]})
 
 const User = new mongoose.model("User", userSchema);
+
+
 
 app.get("/", function (req, res) {
   res.render("home");
@@ -35,16 +39,20 @@ app.route("/login")
 
 .post(function (req, res) {
   const username = req.body.username;
-  const password= req.body.password;
+  const password = req.body.password;
 
   User.findOne({email: username}, function (err, foundUser) {
     if(err){
       console.log(err)
     } else {
       if(foundUser){
-        if (foundUser.password === password){
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if(result = true){
             res.render("secrets");
-        }
+
+          }
+});
+
       }
     }
   })
@@ -57,18 +65,24 @@ app.route("/register")
 })
 
 .post(function (req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+
+    newUser.save(function(err) {
+      if(err){
+        console.log(err)
+      } else {
+        res.render("secrets")
+      }
+    })
   });
 
-  newUser.save(function(err) {
-    if(err){
-      console.log(err)
-    } else {
-      res.render("secrets")
-    }
-  })
+
+
+
 });
 
 
